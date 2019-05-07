8225ce761910959ae7af72f177415143b76a06b6
sugare <30733705@qq.com>
2019-05-07 14:31:15 +0800

    feat(*): add logger and custom url

--- src/main/java/com/song/DemociApplication.java
+++ src/main/java/com/song/DemociApplication.java
@@ -1,20 +1,37 @@
 package com.song;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
 
 @SpringBootApplication
 @RestController
 public class DemociApplication {
 
+	private static final Logger logger = LoggerFactory.getLogger(DemociApplication.class);
+
 	public static void main(String[] args) {
 		SpringApplication.run(DemociApplication.class, args);
 	}
-	
+
 	@GetMapping("/")
 	public String index() {
-		return "<h1>Hello, 麦子 aaa -- Caicloud</h1>";
+		return str("caicloud");
 	}
+
+	@GetMapping("/{str}")
+	public String str(@PathVariable(value = "str") String str) {
+
+		logger.info("The content you enter is: "+str);
+		return "<h1>Hello, "
+				+ str +
+				" -- Caicloud</h1>";
+	}
+
 }
